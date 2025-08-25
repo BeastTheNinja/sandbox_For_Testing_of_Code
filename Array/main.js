f019c5e3fc070f75d9feef156d6f3999aef306a3
BeastTheNinja <134609528+BeastTheNinja@users.noreply.github.com>
2025-08-25 09:01:12 +0200

    Array Testing

---
+++ Array/main.js
@@ -0,0 +1,11 @@
+// sandbox testing of arrays with reverse and filter
+
+const numbers = [1, 2, 3, 4, 5];
+
+// Reverse the array
+const reversed = numbers.reverse();
+console.log('Reversed:', reversed);
+
+// Filter the array
+const filtered = numbers.filter(num => num > 2);
+console.log('Filtered:', filtered);
